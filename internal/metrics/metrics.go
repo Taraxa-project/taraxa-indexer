a6a5737e7c5ddaab7cf1128f2c92c436a98838e2
rjonczy <robert.jonczy@gmail.com>
2023-03-14 15:23:52 +0100

    added metricsPrefix

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -8,26 +8,32 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const metricPrefix = "taraxa_indexer"
+
 var (
 	StorageAddCounter = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "taraxa_indexer_storage_add_counter",
+		Name: metricPrefix + "_storage_add_counter",
 		Help: "The total number of Add operations to storage",
 	})
 	StorageGetCounter = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "taraxa_indexer_storage_get_counter",
+		Name: metricPrefix + "_storage_get_counter",
 		Help: "The total number of Get operations to storage",
 	})
 	RpcCallsCounter = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "taraxa_indexer_rpc_calls_counter",
+		Name: metricPrefix + "_rpc_calls_counter",
 		Help: "The total number of RPC calls to taraxa-node",
 	})
 	IndexedBlocksCounter = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "taraxa_indexer_blocks_indexed_counter",
+		Name: metricPrefix + "_blocks_indexed_counter",
 		Help: "Number of indexed blocks by indexer since restart",
 	})
 	IndexedBlocksTotal = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "taraxa_indexer_blocks_indexed_total",
+		Name: metricPrefix + "_blocks_indexed_total",
 		Help: "Total number of indexed blocks",
+	})
+	IndexedPeriodsTotal = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: metricPrefix + "_periods_indexed_total",
+		Help: "Total number of indexed periods",
 	})
 )
 
