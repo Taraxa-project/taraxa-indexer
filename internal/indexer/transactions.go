21550320781a5b9e8b1e20d1ff9103bcf0d3ce0a
VargaElod23 <vargaelod23@gmail.com>
2023-06-23 15:06:36 +0300

    implements parsing and saving of a single txs event log

--- internal/indexer/transactions.go
+++ internal/indexer/transactions.go
@@ -16,6 +16,7 @@ func (bc *blockContext) processTransactions(trxHashes *[]string) (err error) {
 	tp.Wait()
 
 	internal_transactions := new(models.InternalTransactionsResponse)
+	logs := new(models.TransactionLogsResponse)
 	for i, trx := range transactions {
 		trx_model := trx.ToModelWithTimestamp(bc.block.Timestamp)
 		bc.SaveTransaction(trx_model)
@@ -27,9 +28,12 @@ func (bc *blockContext) processTransactions(trxHashes *[]string) (err error) {
 				}
 				internal := makeInternal(trx_model, entry)
 				internal_transactions.Data = append(internal_transactions.Data, internal)
+				eventLog := makeEventLog(bc, trx, entry)
+				logs.Data = eventLog
 				bc.SaveTransaction(&internal)
 			}
 			bc.batch.AddToBatchSingleKey(internal_transactions, trx_model.Hash)
+			bc.batch.AddToBatchSingleKey(logs, trx_model.Hash)
 		}
 	}
 	return
@@ -78,8 +82,32 @@ func (bc *blockContext) SaveTransaction(trx *models.Transaction) {
 	bc.batch.AddToBatch(trx, trx.To, to_index)
 }
 
+func (bc *blockContext) SaveEventLog(eventLog *models.EventLog) {
+	log.WithFields(log.Fields{"address": eventLog.Address, "trnxHash": eventLog.TransactionHash}).Trace("Saving Event Log")
+
+	bc.batch.AddToBatch(eventLog, eventLog.TransactionHash, eventLog.LogIndex)
+}
+
 func (bc *blockContext) addAddressStatsToBatch() {
 	for _, stats := range bc.addressStats {
 		bc.batch.AddToBatch(stats, stats.Address, 0)
 	}
 }
+
+func makeEventLog(bc *blockContext, trx chain.Transaction, entry chain.TraceEntry) (logs []models.EventLog) {
+	for _, log := range trx.Logs {
+		log := models.EventLog{
+			LogIndex: log.LogIndex,
+			Address: log.Address,
+			Data: log.Data,
+			Removed: log.Removed,
+			Topics: log.Topics,
+			TransactionHash: log.TransactionHash,
+			TransactionIndex: log.TransactionIndex,
+		}
+		bc.SaveEventLog(&log)
+		logs = append(logs, log)
+	}
+	return logs
+}
+
