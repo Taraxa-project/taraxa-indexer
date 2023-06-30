76d1d04fb8be70053030ec600ba9f6c9358e6777
VargaElod23 <elod.varga@taraxa.io>
2023-06-30 17:04:36 +0300

    fixes balances utils, adds tests

--- internal/indexer/balances.go
+++ internal/indexer/balances.go
@@ -1,32 +1,42 @@
 package indexer
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/Taraxa-project/taraxa-indexer/internal/chain"
-	"github.com/Taraxa-project/taraxa-indexer/internal/storage"
 	"github.com/Taraxa-project/taraxa-indexer/internal/utils"
 	"github.com/Taraxa-project/taraxa-indexer/models"
 )
 
-func UpdateBalancesInternal(storage storage.Storage, trx models.Transaction) (err error) {
-	accounts := storage.GetAccounts()
+func UpdateBalancesInternal(ptr *[]models.Account, trx models.Transaction) (err error) {
 
+	accounts := *ptr
 	parsedValue, ok := new(big.Int).SetString(trx.Value, 10)
 
 	if ok && parsedValue.Cmp(big.NewInt(0)) == 1 {
-		fromBalance, _ := utils.FindBalance(accounts, trx.From)
-		toBalance, _ := utils.FindBalance(accounts, trx.To)
-		utils.SubstractFromBalance(fromBalance, *parsedValue)
-		utils.AddToBalance(toBalance, *parsedValue)
+		i := utils.FindBalance(ptr, trx.From)
+		fmt.Println(accounts[i])
+		if i == -1 {
+			utils.RegisterBalance(ptr, trx.From)
+		}
+		j := utils.FindBalance(ptr, trx.To)
+		if j == -1 {
+			utils.RegisterBalance(ptr, trx.To)
+		}
+		utils.SubstractFromBalance(&accounts[i], *parsedValue)
+		if utils.IsZero(*&accounts[i]) {
+			utils.RemoveBalance(ptr, trx.From)
+		}
+		utils.AddToBalance(&accounts[j], *parsedValue)
 	}
 	return
 }
 
-func UpdateBalances(storage storage.Storage, trx *chain.Transaction) error {
-	accounts := storage.GetAccounts()
-
+func UpdateBalances(ptr *[]models.Account, trx *chain.Transaction) error {
 	logs := trx.ExtractLogs()
+
+	accounts := *ptr
 
 	if len(logs) > 0 {
 		events, err := utils.DecodeRewardsTopics(logs)
@@ -34,24 +44,18 @@ func UpdateBalances(storage storage.Storage, trx *chain.Transaction) error {
 			return err
 		}
 		for _, event := range events {
-			account, _ := utils.FindBalance(accounts, event.Account)
-			utils.AddToBalance(account, *event.Value)
-			if utils.IsZero(*account) {
-				utils.RemoveBalance(&accounts, event.Account)
-			}
+			i := utils.FindBalance(ptr, event.Account)
+			utils.AddToBalance(&accounts[i], *event.Value)
 		}
 	}
 	parsedValue, ok := new(big.Int).SetString(trx.Value, 10)
 	if ok && parsedValue.Cmp(big.NewInt(0)) == 1 {
-		fromBalance, _ := utils.FindBalance(accounts, trx.From)
-		toBalance, _ := utils.FindBalance(accounts, trx.To)
-		utils.SubstractFromBalance(fromBalance, *parsedValue)
-		utils.AddToBalance(toBalance, *parsedValue)
-		if utils.IsZero(*fromBalance) {
-			utils.RemoveBalance(&accounts, trx.From)
-		}
-		if utils.IsZero(*toBalance) {
-			utils.RemoveBalance(&accounts, trx.To)
+		j := utils.FindBalance(ptr, trx.From)
+		z := utils.FindBalance(ptr, trx.To)
+		utils.SubstractFromBalance(&accounts[j], *parsedValue)
+		utils.AddToBalance(&accounts[z], *parsedValue)
+		if utils.IsZero(accounts[j]) {
+			utils.RemoveBalance(ptr, trx.From)
 		}
 	}
 	return nil
---
+++ internal/indexer/balances_test.go
@@ -0,0 +1,113 @@
+package indexer
+
+import (
+	"testing"
+
+	"github.com/Taraxa-project/taraxa-indexer/internal/chain"
+	"github.com/Taraxa-project/taraxa-indexer/internal/utils"
+	"github.com/Taraxa-project/taraxa-indexer/models"
+)
+
+func TestUpdateBalancesInternal(t *testing.T) {
+	// Prepare test data
+	accounts := []models.Account{
+		{
+			Address: "0x1111111111111111111111111111111111111111",
+			Balance: "100",
+		},
+		{
+			Address: "0x0DC0d841F962759DA25547c686fa440cF6C28C61",
+			Balance: "50",
+		},
+	}
+	trx := models.Transaction{
+		From:  "0x1111111111111111111111111111111111111111",
+		To:    "0x0DC0d841F962759DA25547c686fa440cF6C28C61",
+		Value: "20",
+	}
+
+	// Invoke the method
+	err := UpdateBalancesInternal(&accounts, trx)
+
+	// Validate the result
+	if err != nil {
+		t.Errorf("UpdateBalancesInternal failed with error: %v", err)
+	}
+
+	// Validate the updated balances
+	i := utils.FindBalance(&accounts, "0x1111111111111111111111111111111111111111")
+	t.Log(accounts[i])
+	if i == -1 || accounts[i].Balance != "80" {
+		t.Error("UpdateBalancesInternal failed to update 'from' balance correctly")
+	}
+
+	j := utils.FindBalance(&accounts, "0x0DC0d841F962759DA25547c686fa440cF6C28C61")
+	t.Log(accounts[j])
+	if j == -1 || accounts[j].Balance != "70" {
+		t.Error("UpdateBalancesInternal failed to update 'to' balance correctly")
+	}
+}
+
+func TestUpdateBalances(t *testing.T) {
+	// Prepare test data
+	accounts := []models.Account{
+		{
+			Address: "0x1111111111111111111111111111111111111111",
+			Balance: "100",
+		},
+		{
+			Address: "0x0DC0d841F962759DA25547c686fa440cF6C28C61",
+			Balance: "50",
+		},
+	}
+	trx := &chain.Transaction{
+		Logs: []chain.EventLog{{
+
+			Address:          "0x00000000000000000000000000000000000000fe",
+			Data:             "0x000000000000000000000000000000000000000000000005d9da3b556bb3aa86",
+			LogIndex:         "0",
+			Removed:          false,
+			Topics:           []string{"0x9310ccfcb8de723f578a9e4282ea9f521f05ae40dc08f3068dfad528a65ee3c7", "0x00000000000000000000000021db400dcb1ef3bc3aee4f3d028ec1939b7fadd6", "0x0000000000000000000000004beaf4ce3c239ac7195a1e422725c0465271fb42"},
+			TransactionHash:  "0xd8c9296770c696b313128f1cc913b1a5e90ddc62b049ceb8a476b1125d65d3a4",
+			TransactionIndex: "1",
+		},
+			{
+				Address:  "0x00000000000000000000000000000000000000fe",
+				Data:     "0x000000000000000000000000000000000000000000000000a7a44a964be1f30a",
+				LogIndex: "0",
+				Removed:  false,
+				Topics: []string{
+					"0xf0ec9e0f6add850a1738c5822244e26ffc3d1f14da7537aa240582b25af12ad0",
+					"0x0000000000000000000000000dc0d841f962759da25547c686fa440cf6c28c61",
+					"0x000000000000000000000000ed4d5f4f3641cbc056e466d15dbe2403e38056f8",
+				},
+				TransactionHash:  "0xe667503bfec2ade69c5e03398aa29a88e035931cadd2caf265c0c85345f3f40e",
+				TransactionIndex: "105",
+			},
+		},
+		Transaction: models.Transaction{
+			Value: "30",
+			From:  "0x1111111111111111111111111111111111111111",
+			To:    "0x0DC0d841F962759DA25547c686fa440cF6C28C61",
+		},
+	}
+
+	// Invoke the method
+	err := UpdateBalances(&accounts, trx)
+
+	// Validate the result
+	if err != nil {
+		t.Errorf("UpdateBalances failed with error: %v", err)
+	}
+
+	// Validate the updated balances
+	i := utils.FindBalance(&accounts, "0x1111111111111111111111111111111111111111")
+	if i == -1 || accounts[i].Balance != "70" {
+		t.Error("UpdateBalances failed to update 'from' balance correctly")
+	}
+
+	j := utils.FindBalance(&accounts, "0x0DC0d841F962759DA25547c686fa440cF6C28C61")
+	if j == -1 || accounts[j].Balance != "80" {
+		t.Error("UpdateBalances failed to update 'to' balance correctly")
+	}
+}
